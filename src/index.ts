5c24b496e36e362e2575e761d7ecd75207aad469
papercusp-git-sync <papercupai@users.noreply.github.com>
2026-08-15 19:39:33 -0400

    sync(.papercusp,.vitest-tmp,root,src): 20 files, +0/-9384 [skip ci]
    chore(git-sync): auto-commit papercusp [skip ci]

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as GitGraphPanel, type GitGraphPanelProps, type Worktree } from './GitGraphPanel';
-export { CommitDetail } from './CommitDetail';
-export { fetchGitLog, getCachedGitLog, preloadGitLog } from './cache';
-export { assignLanes, laneColor } from './graphLayout';
-export type { Commit, LaidOutCommit } from './graphLayout';
